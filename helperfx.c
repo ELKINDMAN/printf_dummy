a49dafea5325d17fe998782644582e9be5ab4073
ELKINDMAN <elkanahkindness@gmail.com>
2023-09-18 20:40:45 +0100

    added more functions to helperfx.c

--- helperfx.c
+++ helperfx.c
@@ -8,5 +8,34 @@
 
 int hastro_int(va_list elkind)
 {
-	return va_arg(elkind, int);
+	int i_len;
+
+	i_len = _ourputchar(va_arg(elkind, int));
+	return (i_len);
+	
+}
+
+int kind_pstr(va_list elkind)
+{
+	int wds;
+	char *str;
+
+	str = va_arg(elkind, char);
+	
+	while (*str != '\0')
+	{
+		_ourputchar(*str);
+		str++;
+	}
+	wds = strlen(str);
+	
+	return (wds);
+}
+
+int hastro_pchcar(va_list elkind)
+{
+	char char_in_elkind = va_arg(elkind, int);
+
+	char_in_elkind = _ourputchar(char_in_elkind);
+	return (0);
 }
